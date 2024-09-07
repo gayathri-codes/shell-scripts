603dd22c41b57434379afbc7f3dd5421b82bb29c
gayathriballap <gayathriballap@gmail.com>
2024-09-07 17:28:38 +0530

    204.236.221.113

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ curl -o /tmp/frontend.zip https://robo/shop-artifacts.s3.amazonaws.com/frontend.
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-cp Users/apple/git-repos/shell-scripts/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+# cp Users/apple/git-repos/shell-scripts/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl start nginx &>>/tmp/roboshop.log 
