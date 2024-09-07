4d4155ef7aa4fd86f9d633fde97c08df23139124
gayathriballap <gayathriballap@gmail.com>
2024-09-07 18:00:38 +0530

    ---

--- frontend.sh
+++ frontend.sh
@@ -4,10 +4,10 @@ yum install nginx -y #&>>/tmp/roboshop.log
 
 rm -rf /usr/share/nginx/html/* #&>>/tmp/roboshop.log
 
-curl -o /tmp/frontend.zip https://robo/shop-artifacts.s3.amazonaws.com/frontend.zip #&>>/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip #&>>/tmp/roboshop.log
 
 cd /usr/share/nginx/html #&>>/tmp/roboshop.log
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip #&>>/tmp/roboshop.log
 
 # cp Users/apple/git-repos/shell-scripts/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
